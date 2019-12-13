3cc65fe1d63c89885383a1a1a060441233a7db47
Tom Boissonnet <tom.boissonnet@embl.it>
2019-12-13 15:13:45 +0100

    Temporal fit

--- theonerig/_nbdev.py
+++ theonerig/_nbdev.py
@@ -25,6 +25,7 @@ index = {"DataChunk": "00_core.ipynb",
          "sum_of_2D_gaussian": "03_modelling.ipynb",
          "fit_nonlinearity": "03_modelling.ipynb",
          "fit_spatial_sta": "03_modelling.ipynb",
+         "fit_temporal_sta": "03_modelling.ipynb",
          "plot_2d_sta": "04_plotting.ipynb",
          "plot_cross_correlation": "04_plotting.ipynb"}
 
